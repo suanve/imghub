af0f08d860fa5554ff87bb17faf087cc57b728f3
suanve <suanwell@hotmail.com>
2021-03-30 00:03:40 +0800

    修复了浏览图片时 文件不存在导致的无限重定向问题🚀

--- web/Common/lib.go
+++ web/Common/lib.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -38,4 +39,16 @@ func RandInt64(min, max int) int {
 		return max
 	}
 	return rand.Intn(max-min) + min
+}
+
+// 判断文件是否存在
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
